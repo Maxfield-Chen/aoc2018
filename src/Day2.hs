e062646e60c035be68096a2a06b02596371d330d
maxfield-chen <maxfieldchen@gmail.com>
2019-11-30 14:46:34 -0800

    Solution to Day2 part 2 using Maybe. Definitely should be refactored. Looking into ghost of departed proofs method to avoid.

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,6 +1,7 @@
 module Day2 where
 
 import           Text.ParserCombinators.Parsec
+import Data.Maybe as Maybe
 import qualified Data.Map                      as M
 
 pIds :: Parser [String]
@@ -30,10 +31,56 @@ genChecksum = foldl
   )
   (0, 0)
 
+lettersDiffering :: String -> String -> Int
+lettersDiffering = countDiff 0
+ where
+  countDiff n a b | null a || null b = n
+                  | x == x'          = countDiff n xs xs'
+                  | x /= x'          = countDiff (n + 1) xs xs'
+                  | otherwise        = -1
+   where
+    (x , xs ) = (head a, tail a)
+    (x', xs') = (head b, tail b)
+
+intersection :: String -> String -> String
+intersection = doIntersection ""
+ where
+  doIntersection ret a b | null a || null b = ret
+                         | x == x'          = doIntersection (ret ++ [x]) xs xs'
+                         | x /= x'          = doIntersection ret xs xs'
+   where
+    (x , xs ) = (head a, tail a)
+    (x', xs') = (head b, tail b)
+
+takeFirst :: (a -> Bool) -> [a] -> Maybe a
+takeFirst f a  = case filter f a of 
+                   (x:_) -> Just (head (filter f a))
+                   [] -> Nothing
+
+firstIntersectionN :: Int -> [String] -> String -> Maybe String
+firstIntersectionN n pool candidate = takeFirst (not . null) intersections
+ where
+  intersections = map (differsByN n candidate) pool
+  differsByN n a b
+    | lettersDiffering a b == n = intersection a b
+    | otherwise = ""
+
+findOffByN :: Int -> [String] -> [String] -> Maybe String
+findOffByN n a a' = Maybe.fromMaybe Nothing ret
+  where validIntersections = map (firstIntersectionN n a') a
+        ret = takeFirst (not . null) validIntersections
+
+printFirstOffsetId :: IO ()
+printFirstOffsetId = do
+  idData <- readFile "/home/nihliphobe/projects/haskell/aoc2018/data/day2.txt"
+  case parse pIds "(unknown)" idData of
+    Left  err -> fail (show err)
+    Right ids -> print (findOffByN 1 ids ids)
+
 printCheckSum :: IO ()
 printCheckSum = do
   idData <- readFile "/home/nihliphobe/projects/haskell/aoc2018/data/day2.txt"
   case parse pIds "(unknown)" idData of
-    Left err -> fail (show err)
-    Right ids -> print $ threeCount * fourCount 
+    Left  err -> fail (show err)
+    Right ids -> print $ threeCount * fourCount
       where (threeCount, fourCount) = genChecksum ids
